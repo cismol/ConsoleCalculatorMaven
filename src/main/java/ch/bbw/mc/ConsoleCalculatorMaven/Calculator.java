34f174a5756b3935133a8adb803f343bd4acb0b2
Cisco.Molnar <ciscimolnar@gmail.com>
2018-11-30 08:34:41 +0100

    All new Classes created

---
+++ src/main/java/ch/bbw/mc/ConsoleCalculatorMaven/Calculator.java
@@ -0,0 +1,9 @@
+package ch.bbw.mc.ConsoleCalculatorMaven;
+
+public class Calculator {
+
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/ch/bbw/mc/ConsoleCalculatorMaven/TestCalculator.java
@@ -0,0 +1,9 @@
+package ch.bbw.mc.ConsoleCalculatorMaven;
+
+public class TestCalculator {
+
+	public TestCalculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
