f997b39c772e4de8f2268edef039fac009c4efa7
Cisco.Molnar <ciscimolnar@gmail.com>
2018-11-30 08:49:49 +0100

    Implementet Calculator in Application class

--- src/main/java/ch/bbw/mc/ConsoleCalculatorMaven/CalculatorApp.java
+++ src/main/java/ch/bbw/mc/ConsoleCalculatorMaven/CalculatorApp.java
@@ -1,13 +1,24 @@
 package ch.bbw.mc.ConsoleCalculatorMaven;
 
 /**
- * Hello world!
+ * Console Calculator
+ * @author Cisco Molnár
  *
  */
 public class CalculatorApp 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculator calculator = new Calculator();
+        int valueA = 0;
+        int valueB = 0;
+        
+        System.out.println("Colsole calculator");
+        System.out.println("==================");
+        System.out.println();
+        
+        valueA = 10;
+        valueB = 20;
+        System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(valueA, valueB));
     }
 }
