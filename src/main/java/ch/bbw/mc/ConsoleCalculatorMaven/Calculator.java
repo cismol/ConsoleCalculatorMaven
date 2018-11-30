94b2952fdc23af3b0273b7a82fab4cb11e1751e5
Cisco.Molnar <ciscimolnar@gmail.com>
2018-11-30 08:44:25 +0100

    Added function summe

--- src/main/java/ch/bbw/mc/ConsoleCalculatorMaven/Calculator.java
+++ src/main/java/ch/bbw/mc/ConsoleCalculatorMaven/Calculator.java
@@ -1,9 +1,20 @@
 package ch.bbw.mc.ConsoleCalculatorMaven;
 
+/**
+ *  Calculator
+ *  - Kann 2 Zahlen addieren
+ * 
+ * 
+ */
+
 public class Calculator {
 
 	public Calculator() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public int summe(int summand1, int summand2){
+		return summand1 + summand2;
 	}
 
 }
